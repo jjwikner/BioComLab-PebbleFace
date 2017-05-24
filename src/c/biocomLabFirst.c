05dedc1825f76bacf8f7c0b08c742547a0b6818b
J Jacob Wikner <jjwikner@gmail.com>
2017-05-24 16:19:33 +0200

    Added comments and shaker detector

--- src/c/biocomLabFirst.c
+++ src/c/biocomLabFirst.c
@@ -3,6 +3,12 @@
 
 // #include <unistd.h>
 
+// Originating from the hello world-concept to illustrate and find conceptual limits with the pebble equipment.
+// 
+// JJ Wikner 
+// BioCom Lab
+//
+
 static Window *s_window;
 static TextLayer *s_text_layer;
 
@@ -13,24 +19,46 @@ static void deinit(void) {
 	window_destroy(s_window);
 }
 
+// Vibe pattern: ON for 200ms, OFF for 100ms, ON for 400ms:
+static const uint32_t const segments[] = { 200, 100, 400 };
+VibePattern pat = {
+  .durations = segments,
+  .num_segments = ARRAY_LENGTH(segments),
+};
+
+
 static void biocomSelectSingleClickHandler(ClickRecognizerRef recognizer, void *context) {
-	// A single click has just occured
+	// Detect that a single click has just occured. 
+	// Notice that the binders is independent of the button clicked.
 	deinit();
 }
 
 static void biocomUpSingleClickHandler(ClickRecognizerRef recognizer, void *context) {
 	// A single click has just occured
+	// In this case, create a buzzing sound to illustrate this
 	vibes_long_pulse();
 }
 
-static void biocomClickConfigProvider(void *context) { // Rename biocom	
-	// Subscribe to button click events here
-	ButtonId bidSelect = BUTTON_ID_SELECT; // The select button
-	window_single_click_subscribe(bidSelect, biocomSelectSingleClickHandler); // The trigger event
-	ButtonId bidUp = BUTTON_ID_UP;
-	window_single_click_subscribe(bidUp, biocomUpSingleClickHandler);
+static void biocomDownSingleClickHandler(ClickRecognizerRef recognizer, void *context) {
+	// A single click has just occured
+	// In this case, create a buzzing sound to illustrate this
+	// vibes_long_pulse();
+	vibes_enqueue_custom_pattern(pat);
 }
 
+static void biocomClickConfigProvider(void *context) { // Rename biocom	
+	// Subscribe to button click events here	
+	// They are contextually bound to the home screen
+	
+	ButtonId bidSelect = BUTTON_ID_SELECT; // The select button
+	window_single_click_subscribe(bidSelect, biocomSelectSingleClickHandler); // The trigger event
+	
+	ButtonId bidUp = BUTTON_ID_UP;
+	window_single_click_subscribe(bidUp, biocomUpSingleClickHandler);
+
+	ButtonId bidDn = BUTTON_ID_DOWN;
+	window_single_click_subscribe(bidDn, biocomDownSingleClickHandler);
+}
 
 void biocomPutHomeString(char *windowString) {
 	// Create a window and get information about the window
@@ -52,8 +80,8 @@ void biocomPutHomeString(char *windowString) {
 	// Add the text layer to the window
 	layer_add_child(window_get_root_layer(s_window), text_layer_get_layer(s_text_layer));
   
-  // Enable text flow and paging on the text layer, with a slight inset of 10, for round screens
-  text_layer_enable_screen_text_flow_and_paging(s_text_layer, 10);
+  	// Enable text flow and paging on the text layer, with a slight inset of 10, for round screens
+  	text_layer_enable_screen_text_flow_and_paging(s_text_layer, 10);
 
 	// Push the window, setting the window animation to 'true'
 	window_stack_push(s_window, true);
@@ -62,10 +90,19 @@ void biocomPutHomeString(char *windowString) {
 	APP_LOG(APP_LOG_LEVEL_DEBUG, "Just pushed a window!");
 }
 
+static void accel_tap_handler(AccelAxisType axis, int32_t direction) {
+  	// A tap event occured
+	biocomPutHomeString("Shaky!");
+	psleep(1);
+	biocomPutHomeString("BioCom Lab - Wristband communicator");
+	
+}
+
+
 static void init(void) {
 	biocomPutHomeString("BioCom Lab - Wristband communicator");
-	psleep(500);
-
+	// psleep(500);
+	accel_tap_service_subscribe(accel_tap_handler);
 }
 
 int main(void) {
